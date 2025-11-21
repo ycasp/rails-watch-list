8925895a5aeb5939fac4694c89ce1a9345b322ea
Yannik Caspar <y.caspar@bluewin.ch>
2025-11-21 12:29:50 +0100

    did the root rout to the list index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "lists#index"
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
